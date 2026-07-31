f096c90e14b7658faa0837c9acfae3305637976c
josem <josem@jmaledom.local>
2026-07-31 11:00:20 +0200

    Version social preview URLs

--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,4 @@
 export const SITE_TITLE = 'Trazli';
 export const SITE_DESCRIPTION = 'Todo lo tuyo, en un solo enlace.';
+export const SOCIAL_IMAGE_VERSION = '20260731';
 export const DEFAULT_PAYPAL_URL = 'https://paypal.me/jmaledom';
